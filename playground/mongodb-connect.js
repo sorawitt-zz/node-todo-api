5bae7c7b40d3284df386895f44c42fe1c3886115
sorawit <sorawit.tr@gmail.com>
2018-06-17 16:07:27 +0700

    delete comment code

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -8,29 +8,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to Mango DB server');
 
     const db = client.db('TodoApp');
-    
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert todo', err);
-    //     }
-
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
-
-    // db.collection('Users').insertOne({
-    //     name: 'Sorawit',
-    //     age: 22,
-    //     location: 'Sukhothai'
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert Users', err);
-    //     }
-
-    //     console.log(result.ops);
-    // });
 
     db.collection('Users').find({
         age: 22
